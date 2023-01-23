dfdbd57c3aa3824bc70be646950d7d14209a2a25
Nuno Souto <nsouto@lostlink.net>
2023-01-22 19:45:17 -0500

    Headers Tailwind styling
    Signed-off-by: Nuno Souto <nsouto@lostlink.net>

--- resources/views/blocks/header.blade.php
+++ resources/views/blocks/header.blade.php
@@ -1,5 +1,16 @@
 @php
-$tag = "h" . $data['level'] ?? 1;
+    $level = (int) ($data['level'] ?? 1);
+    $tag = "h{$level}";
+
+    $class = match($level) {
+        1 => 'px-0 pt-2 pb-1 mx-0 mt-0 mb-3 text-4xl font-extrabold text-gray-900 break-words',
+        2 => 'px-0 pt-2 pb-1 mx-0 mt-6 mb-3 text-2xl font-bold text-gray-900 break-words',
+        3 => 'px-0 pt-2 pb-1 mx-0 mt-6 mb-2 text-xl font-semibold text-gray-900 break-words',
+        4 => 'px-0 pt-2 pb-1 mx-0 mt-6 mb-2 font-semibold text-gray-900 break-words',
+        5 => 'px-0 pt-2 pb-1 m-0 leading-5 text-gray-700 break-words',
+        6 => 'px-0 pt-2 pb-1 m-0 leading-5 text-gray-700 break-words',
+        default => ''
+    };
 @endphp
 
-<{{ $tag }}>{{ $data['text'] ?? '' }}</{{ $tag }}>
+<{{ $tag }} class="{{ $class }}">{{ $data['text'] ?? '' }}</{{ $tag }}>
